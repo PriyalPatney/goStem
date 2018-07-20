49edff464dbe16fff5e8ea45d75869428e8a884b
PriyalPatney <34459102+PriyalPatney@users.noreply.github.com>
2018-07-21 00:45:15 +0530

    folders added

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/goStem");
+
+
+
+app.listen(3000, function(){
+    console.log("The Server Has Started!");
+ });
