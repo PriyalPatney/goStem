f334c336a4207f395c07e3fa6f960d509205439c
PriyalPatney <34459102+PriyalPatney@users.noreply.github.com>
2018-07-22 08:07:49 +0530

    navbar added

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+    app = express();
+    bodyParser = require("body-parser");
+    mongoose = require("mongoose");
+    methodOverride  = require("method-override");
+    
 
-mongoose.connect("mongodb://localhost:27017/goStem");
+mongoose.connect("mongodb://localhost:27017/goStem",{ useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+
+// ROUTES
+app.get("/", function(req,res){
+    res.render("landing");
+})
 
 
 app.listen(3000, function(){
-    console.log("The Server Has Started!");
+    console.log("The Server Has Started!!!!");
  });
